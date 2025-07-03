b1f79ee3d7f4817e8d55ffc77258e7a09c91384d
Diounata <jonathamcordeiro42@gmail.com>
2025-07-02 23:57:30 -0300

    fix: fix theme switch location in authentication layout

--- src/app/(public)/autenticacao/layout.tsx
+++ src/app/(public)/autenticacao/layout.tsx
@@ -4,9 +4,12 @@ import { ThemeSwitch } from "@/components/theme-switch";
 
 export default function AuthenticationLayout({ children }: PropsWithChildren) {
   return (
-    <div className="flex min-h-screen w-screen flex-col items-center justify-center bg-white sm:bg-gray-100 dark:bg-zinc-900 dark:sm:bg-zinc-800">
+    <div className="flex min-h-screen w-screen flex-col items-center justify-center bg-white dark:bg-zinc-900 sm:bg-gray-100 dark:sm:bg-zinc-800">
       {children}
-      <ThemeSwitch />
+
+      <div className="absolute bottom-6 right-6">
+        <ThemeSwitch />
+      </div>
     </div>
   );
 }
